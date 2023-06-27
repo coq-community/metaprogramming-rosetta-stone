630815fe4bfddd43db80c86d1c335cdc430d38a1
Talia Ringer <talia@dependenttyp.es>
2023-06-27 04:24:36 -0500

    1, 2, and 3 argument versions

--- Custom.v
+++ Custom.v
@@ -2,6 +2,10 @@
  * This is a modification of a tactic implemented for CS 598 on
  * proof automation intially.
  *
+ * The biggest limitation of the Ltac approach is that we must limit
+ * the number of arguments we can apply it to. We can't easily build
+ * a version of this tactic that works on arbitrarily many arguments.
+ *
  * We'll build on some tactics from StructTact, and also build a new tactic
  * in the style of StructTact. This imports the StructTact library so we can do that:
  *)
@@ -51,11 +55,19 @@ Ltac in_reduced f t :=
 (*
  * The tactic in_reduced_f matches over a goal, finds a function f applied to arguments
  * x and y, and then applies tactical t to x, y, and the reduced f.
+ *
+ * Here the limitations of Ltac really shine. I stop at 3 arguments
+ * because I'm impatient and I'm stuck handling each number of arguments
+ * separately because of Ltac.
  *)
 Ltac in_reduced_f f t :=
   match goal with
+  | [ |- context [ f ?x ?y ?z ] ] =>
+    in_reduced f ltac:(t x y z)
   | [ |- context [ f ?x ?y ] ] =>
     in_reduced f ltac:(t x y)
+  | [ |- context [ f ?x ] ] =>
+    in_reduced f ltac:(t x) 
   end.
 
 Module V1.
@@ -64,7 +76,14 @@ Module V1.
  * The first version of autoinduct makes some extra assumptions, but doesn't rely on
  * StructTact or anything else.
  *)
-Ltac autoinduct f :=
+Ltac autoinduct1 f :=
+  in_reduced_f f ltac:(fun x f =>
+    lazymatch f with
+    | (fix f n {struct n} := @?body f n) =>
+      induction x
+    end).
+
+Ltac autoinduct2 f :=
   in_reduced_f f ltac:(fun x y f =>
     lazymatch f with
     | (fix f n m {struct m} := @?body f n m) =>
@@ -72,6 +91,19 @@ Ltac autoinduct f :=
     | (fix f n m {struct n} := @?body f n m) =>
       induction x
     end).
+
+Ltac autoinduct3 f :=
+  in_reduced_f f ltac:(fun x y z f =>
+    lazymatch f with
+    | (fix f n m p {struct p} := @?body f n m p) =>
+      induction z
+    | (fix f n m p {struct m} := @?body f n m p) =>
+      induction y
+    | (fix f n m p {struct n} := @?body f n m p) =>
+      induction x
+    end).
+
+Ltac autoinduct f := first [autoinduct1 f | autoinduct2 f | autoinduct3 f]. 
 
 (* --- Some tests --- *) 
 
@@ -132,7 +164,14 @@ Module V2.
  * some of them.
  *)
 
-Ltac autoinduct_body f :=
+Ltac autoinduct1 f :=
+  in_reduced_f f ltac:(fun x f =>
+    lazymatch f with
+    | (fix f n {struct n} := @?body f n) =>
+      try (rememberNonVars x); generalizeEverythingElse x; induction x
+    end).
+
+Ltac autoinduct2 f :=
   in_reduced_f f ltac:(fun x y f =>
     lazymatch f with
     | (fix f n m {struct m} := @?body f n m) =>
@@ -141,7 +180,18 @@ Ltac autoinduct_body f :=
       try (rememberNonVars x); generalizeEverythingElse x; induction x
     end).
 
-Ltac autoinduct f := intros; autoinduct_body f.
+Ltac autoinduct3 f :=
+  in_reduced_f f ltac:(fun x y z f =>
+    lazymatch f with
+    | (fix f n m p {struct p} := @?body f n m p) =>
+      try (rememberNonVars z); generalizeEverythingElse z; induction z
+    | (fix f n m p {struct m} := @?body f n m p) =>
+      try (rememberNonVars y); generalizeEverythingElse y; induction y
+    | (fix f n m p {struct n} := @?body f n m p) =>
+      try (rememberNonVars x); generalizeEverythingElse x; induction x
+    end).
+
+Ltac autoinduct f := intros; first [autoinduct1 f | autoinduct2 f | autoinduct3 f].
 
 Lemma add_right_comm:
   forall (n m : nat),
@@ -163,45 +213,4 @@ Qed.
 
 End V2.
 
-Module V3.
 
-(*
- * This version is more heuristic but tries to pick a 
- * function automatically, too.
- *)
-Ltac in_reduced_f_guess t :=
-  match goal with
-  | [ |- context [ ?f ?x ?y ] ] =>
-    in_reduced f ltac:(t x y)
-  end.
-
-Ltac autoinduct_body :=
-  in_reduced_f_guess ltac:(fun x y f =>
-    lazymatch f with
-    | (fix f n m {struct m} := @?body f n m) =>
-      try (rememberNonVars y); generalizeEverythingElse y; induction y
-    | (fix f n m {struct n} := @?body f n m) =>
-      try (rememberNonVars x); generalizeEverythingElse x; induction x
-    end).
-
-Ltac autoinduct := intros; autoinduct_body.
-
-Lemma add_right_comm:
-  forall (n m : nat),
-    add_right n m = add_right m n.
-Proof.
-  autoinduct; intros; simpl.
-  - symmetry. apply add_right_O.
-  - find_higher_order_rewrite. apply add_right_S.
-Qed.
-
-Lemma add_left_comm:
-  forall (n m : nat),
-    add_left n m = add_left m n.
-Proof.
-  autoinduct; intros; simpl.
-  - symmetry. apply add_left_O.
-  - find_higher_order_rewrite. apply add_left_S.
-Qed.
-
-End V3.
