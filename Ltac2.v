b76334d3d219fbf301dc6ea2a6b8d472697a323c
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-06-27 15:14:39 +0200

    Ltac2: compute structural argument more eagerly

--- Ltac2.v
+++ Ltac2.v
@@ -1,39 +1,43 @@
 Require Import Ltac2.Ltac2.
 
+(* stdlib fail does enter so has type unit (indeed it doesn't fail if
+   there are no focused goals) *)
+Ltac2 fail s := Control.backtrack_tactic_failure s.
+
+Ltac2 struct_arg f :=
+  match Constr.Unsafe.kind f with
+  | Constr.Unsafe.Fix structs which _ _ => Array.get structs which
+  | _ => fail "not a fixpoint"
+  end.
+
 Ltac2 find_applied f :=
   match! goal with
   | [ |- context [ ?t ] ] =>
       match Constr.Unsafe.kind t with
       | Constr.Unsafe.App g args =>
-          let f :=
+          let farg :=
             match f with
-            | Some f =>
-                if Constr.equal f g then
-                  f
-                else
-                  Control.backtrack_tactic_failure "applies a different function"
+            | Some (f,farg) =>
+                if Constr.equal f g then farg
+                else fail "applies a different function"
             | None =>
-                if Constr.is_const g then g
-                else Control.backtrack_tactic_failure "not a constant"
+                if Constr.is_const g then struct_arg (eval red in $g)
+                else fail "not a constant"
             end
           in
-          let f := eval red in $f in
-          (f, args)
-      | _ => Control.backtrack_tactic_failure "not an application"
+          if Int.le farg (Array.length args)
+          then Array.get args farg
+          else fail "not applied enough"
+      | _ => fail "not an application"
       end
   end.
 
 (* might want to call this in find_reducible_apply so match goal will backtrack *)
-Ltac2 struct_arg f :=
-  match Constr.Unsafe.kind f with
-  | Constr.Unsafe.Fix structs which _ _ => Array.get structs which
-  | _ => Control.backtrack_tactic_failure "not a fixpoint"
-  end.
 
 Ltac2 autoinduct0 f :=
-  let (g, args) := find_applied f in
-  let main_arg := Array.get args (struct_arg g) in
-  induction $main_arg.
+  let f := Option.map (fun f => (f, struct_arg (eval red in $f))) f in
+  let arg := find_applied f in
+  induction $arg.
 
 Ltac2 Notation "autoinduct" f(constr) := (autoinduct0 (Some f)).
 
