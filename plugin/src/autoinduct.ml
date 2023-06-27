6a5f5417398ae280a46e87f982881801168ed9d7
Talia Ringer <talia@dependenttyp.es>
2023-06-27 08:05:21 -0500

    sketch out function inside of monad

--- plugin/src/autoinduct.ml
+++ plugin/src/autoinduct.ml
@@ -1,8 +1,18 @@
+open Proofview
+
 (*
  * Implementation of autoinduct tactic
  *)
+let do_autoinduct env i sigma =
+  let _ = Feedback.msg_warning (Pp.str "Autoinduct is not yet implemented!") in
+  Tacticals.tclIDTAC
+
+(*
+ * Implementation of autoinduct tactic, top level
+ *)
 let autoinduct i =
-  Proofview.Goal.enter begin fun gl ->
-    let _ = Feedback.msg_warning (Pp.str "Autoinduct is not yet implemented!") in
-    Tacticals.tclIDTAC
+  Goal.enter begin fun gl ->
+    let env = Goal.env gl in
+    let sigma = Goal.sigma gl in
+    do_autoinduct env i sigma
   end
