5ef83e9c43b4e7a06406b796f6196d6498f4c990
Enzo Crance <enzo.crance@inria.fr>
2023-06-27 14:33:22 +0200

    :sparkles: Step 2: autoinduct on f, with backtracking when the induction tactic fails

--- elpi/Test.v
+++ elpi/Test.v
@@ -20,46 +20,50 @@ Fixpoint add_right (n m : nat) : nat :=
 Lemma add_left_O : forall (n : nat), add_left n O = n.
 Proof.
   intros.
-  autoinduct on (add_left n O).
+  (* autoinduct on (add_left n O). *)
+  autoinduct on add_left.
   all: (simpl; congruence).
 Qed.
  
 Lemma add_right_O : forall (m : nat), add_right O m = m.
 Proof.
   intros.
-  autoinduct on (add_right O m).
+  (* autoinduct on (add_right O m). *)
+  autoinduct on add_right.
   all: (simpl; congruence).
 Qed.
  
 Lemma add_left_S : forall (n m : nat), S (add_left n m) = add_left n (S m).
 Proof.
   intros.
-  autoinduct on (add_left n m).
+  (* autoinduct on (add_left n m). *)
+  autoinduct on add_left.
   all: (simpl; congruence).
 Qed.
  
 Lemma add_right_S : forall (n m : nat), S (add_right n m) = add_right (S n) m.
 Proof.
   intros.
-  autoinduct on (add_right n m).
+  (* autoinduct on (add_right n m). *)
+  autoinduct on add_right.
   all: (simpl; congruence).
 Qed.
  
-Lemma add_left_comm: forall (n m : nat), add_left n m = add_left m n.
+Lemma add_left_comm : forall (n m : nat), add_left n m = add_left m n.
 Proof.
   intros.
-  autoinduct on (add_left n m).
+  (* autoinduct on (add_left n m). *)
+  autoinduct on add_left.
   all: simpl.
   - symmetry. apply add_left_O.
   - rewrite IHn. apply add_left_S.
 Qed.
 
-Lemma add_right_comm:
-  forall (n m : nat),
-    add_right n m = add_right m n.
+Lemma add_right_comm : forall (n m : nat), add_right n m = add_right m n.
 Proof.
   intros.
-  autoinduct on (add_right n m).
+  (* autoinduct on (add_right n m). *)
+  autoinduct on add_right.
   all: simpl.
   - symmetry. apply add_right_O.
   - rewrite IHm. apply add_right_S.
