80b06c4502cf3cf10023d6cf68fdea9a08017453
Talia Ringer <talia@dependenttyp.es>
2023-06-29 15:52:43 +0200

    add a few more tests

--- autoinduct/Test.v
+++ autoinduct/Test.v
@@ -23,15 +23,55 @@ Module Step0.
 Lemma add_left_O : forall (n : nat), add_left n O = n.
 Proof.
   intros.
-  autoinduct on (add_left n O) || (idtac "step0 fails"; induction n).
+  autoinduct on (add_left n O) || (idtac "step0 test1 fails"; induction n).
   all: (simpl; congruence).
 Qed.
  
 Lemma add_right_O : forall (m : nat), add_right O m = m.
 Proof.
   intros.
-  autoinduct on (add_right O m) || (idtac "step0 fails"; induction m).
+  autoinduct on (add_right O m) || (idtac "step0 test2 fails"; induction m).
   all: (simpl; congruence).
+Qed.
+
+Lemma add_left_S :
+  forall (n m : nat),
+    S (add_left n m) = add_left n (S m).
+Proof.
+  intros.
+  autoinduct on (add_left n m) || (idtac "step0 test3 fails"; induction n).
+  all: (simpl; congruence).
+Qed.
+
+Lemma add_right_S :
+  forall (n m : nat),
+    S (add_right n m) = add_right (S n) m.
+Proof.
+  intros.
+  autoinduct on (add_right n m) || (idtac "step0 test4 fails"; induction m).
+  all: (simpl; congruence).
+Qed.
+
+Lemma add_left_comm:
+  forall (n m : nat),
+    add_left n m = add_left m n.
+Proof.
+  intros.
+  autoinduct on (add_left n m) || (idtac "step0 test5 fails"; induction n).
+  all: simpl.
+  - symmetry. apply add_left_O.
+  - (rewrite IHn || rewrite IHn0). apply add_left_S.
+Qed.
+
+Lemma add_right_comm:
+  forall (n m : nat),
+    add_right n m = add_right m n.
+Proof.
+  intros.
+  autoinduct on (add_right n m) || (idtac "step0 test6 fails"; induction m).
+  all: simpl.
+  - symmetry. apply add_right_O.
+  - (rewrite IHm || rewrite IHn0). apply add_right_S.
 Qed.
 
 End Step0.
@@ -41,15 +81,55 @@ Module Step1.
 Lemma add_left_O : forall (n : nat), add_left n O = n.
 Proof.
   intros.
-  autoinduct on add_left || (idtac "step1 fails"; induction n).
+  autoinduct on add_left || (idtac "step1 test1 fails"; induction n).
   all: (simpl; congruence).
 Qed.
  
 Lemma add_right_O : forall (m : nat), add_right O m = m.
 Proof.
   intros.
-  autoinduct on add_right  || (idtac "step1 fails"; induction m).
+  autoinduct on add_right  || (idtac "step1 test2 fails"; induction m).
   all: (simpl; congruence).
+Qed.
+
+Lemma add_left_S :
+  forall (n m : nat),
+    S (add_left n m) = add_left n (S m).
+Proof.
+  intros.
+  autoinduct on add_left || (idtac "step1 test3 fails"; induction n).
+  all: (simpl; congruence).
+Qed.
+
+Lemma add_right_S :
+  forall (n m : nat),
+    S (add_right n m) = add_right (S n) m.
+Proof.
+  intros.
+  autoinduct on add_right || (idtac "step1 test4 fails"; induction m).
+  all: (simpl; congruence).
+Qed.
+
+Lemma add_left_comm:
+  forall (n m : nat),
+    add_left n m = add_left m n.
+Proof.
+  intros.
+  autoinduct on add_left || (idtac "step1 test5 fails"; induction n).
+  all: simpl.
+  - symmetry. apply add_left_O.
+  - (rewrite IHn || rewrite IHn0). apply add_left_S.
+Qed.
+
+Lemma add_right_comm:
+  forall (n m : nat),
+    add_right n m = add_right m n.
+Proof.
+  intros.
+  autoinduct on add_right || (idtac "step1 test6 fails"; induction m).
+  all: simpl.
+  - symmetry. apply add_right_O.
+  - (rewrite IHm || rewrite IHn0). apply add_right_S.
 Qed.
 
 End Step1.
@@ -59,15 +139,56 @@ Module Step2.
 Lemma add_left_O : forall (n : nat), add_left n O = n.
 Proof.
   intros.
-  autoinduct || (idtac "step2 fails"; induction n).
+  autoinduct || (idtac "step2 test1 fails"; induction n).
   all: (simpl; congruence).
 Qed.
  
 Lemma add_right_O : forall (m : nat), add_right O m = m.
 Proof.
   intros.
-  autoinduct || (idtac "step2 fails"; induction m).
+  autoinduct || (idtac "step2 test2 fails"; induction m).
   all: (simpl; congruence).
 Qed.
+
+Lemma add_left_S :
+  forall (n m : nat),
+    S (add_left n m) = add_left n (S m).
+Proof.
+  intros.
+  autoinduct || (idtac "step2 test3 fails"; induction n).
+  all: (simpl; congruence).
+Qed.
+
+Lemma add_right_S :
+  forall (n m : nat),
+    S (add_right n m) = add_right (S n) m.
+Proof.
+  intros.
+  autoinduct || (idtac "step2 test4 fails"; induction m).
+  all: (simpl; congruence).
+Qed.
+
+Lemma add_left_comm:
+  forall (n m : nat),
+    add_left n m = add_left m n.
+Proof.
+  intros.
+  autoinduct || (idtac "step2 test5 fails"; induction n).
+  all: simpl.
+  - symmetry. apply add_left_O.
+  - (rewrite IHn || rewrite IHn0). apply add_left_S.
+Qed.
+
+Lemma add_right_comm:
+  forall (n m : nat),
+    add_right n m = add_right m n.
+Proof.
+  intros.
+  autoinduct || (idtac "step2 test6 fails"; induction m).
+  all: simpl.
+  - symmetry. apply add_right_O.
+  - (rewrite IHm || rewrite IHn0). apply add_right_S.
+Qed.
+
 
 End Step2.
