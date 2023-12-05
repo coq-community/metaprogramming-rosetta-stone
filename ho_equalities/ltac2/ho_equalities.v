96be9bbb45dec5e3842967558e69cdcabc57f22a
lduboisd <louise.dubois-de-prisque@inria.fr>
2023-12-05 08:54:21 +0100

    working ltac2 version

--- ho_equalities/ltac2/ho_equalities.v
+++ ho_equalities/ltac2/ho_equalities.v
@@ -1,28 +1,6 @@
 From Ltac2 Require Import Ltac2.
 From Ltac2 Require Import Constr.
 Import Unsafe.
-Set Default Proof Mode "Classic".
-Set Printing Existential Instances.
-Set Printing Goal Names.
-
-Goal True.
-let H := fresh in epose (H := ?[x] : ?[Goal]).
-refine (_ _).  Focus 2.
-instantiate (Goal2 := nat).
-
-
-
-
- assert H. unfold H.
-instantiate (x := (forall (A : Type), A)). intros A.
-refine ( _ _). Focus 2. exact H. *) 
-(* 
-Ltac2 rec make_binder_list (l : (ident option * constr) list) :=
-  match l with
-    | [] => []
-    | (id, a) :: l => Binder.unsafe_make id Binder.Relevant a :: make_binder_list l
-  end. *)
-
 
 Ltac2 split_prod (t : constr) :=
   let rec aux t acc :=
@@ -32,85 +10,64 @@ Ltac2 split_prod (t : constr) :=
       | _ => (acc, t)
     end in aux t [].
 
-Ltac2 rec gen_eq (hyp: constr) (binds :(ident option * constr) list) (b: constr) (f : constr) (g : constr) (l: kind list) () :=
-Message.print (Message.of_string "test") ;
+Ltac2 fresh_ident_of_option (opt : ident option) :=
+  match opt with
+    | None => 
+        let idH := ident:(H) in
+        let frees := Fresh.Free.of_ids [ident:(H)] in
+        Fresh.fresh frees idH
+    | Some id => id
+  end.
+
+Ltac2 rec gen_eq (hyp: ident) (binds :(ident option * constr) list) (b: constr) (f : constr) (g : constr) (l: kind list) () :=
   match binds with
     | [] =>
-        let idH := ident:(H) in 
-        let frees := Fresh.Free.of_ids [ident:(H)] in 
         let rels := Array.of_list (List.rev (List.map make l)) in
-        let f := make (App f rels) in Message.print (Message.of_constr f) ;
+        let f := make (App f rels) in 
         let g := make (App g rels) in
         let arr := Array.of_list [b; f; g] in 
         let trm := make (App constr:(@eq) arr) in 
-          ltac1:(term |- instantiate (Goal0 := term)) (Ltac1.of_constr trm)
-        (* assert (id : $trm) *) (* by (rewrite $hyp; reflexivity) ; Std.revert [idH] *)
+        let idH := Fresh.in_goal @H in 
+        let h := Control.hyp hyp in 
+        assert (idH : $trm) by ltac1:(h |- rewrite h ; reflexivity) (Ltac1.of_constr h) ; eapply &idH 
     | (opt, c) :: binds => 
-        let id :=
-          match opt with
-            | None => 
-                let idH := ident:(H) in
-                let frees := Fresh.Free.of_ids [ident:(H)] in
-                Fresh.fresh frees idH
-            | Some id => id
-          end in  Message.print (Message.of_constr c) ;
+        let id := fresh_ident_of_option opt in 
         let newbinds := List.map (fun (x, y) => (x, substnl [make (Var id)] 0 y)) binds in
         let proof := 
-          in_context id c (gen_eq hyp newbinds b f g ((Var id) :: l)) in
-        let frees := Fresh.Free.of_ids [@proof] in
-        let id := Fresh.fresh frees @proof in assert (id := $proof)
+          in_context id c (gen_eq hyp newbinds (substnl [make (Var id)] (List.length binds) b) f g ((Var id) :: l)) in
+        let id :=  Fresh.in_goal @H in assert ($id := $proof) ; ltac1:(proof |- try (exact proof)) (Ltac1.of_constr proof)
       end.
 
-
-Ltac2 eliminate_ho_eq (t : constr) :=
+Ltac2 eliminate_ho_eq (h : ident) :=
+  let t := Control.hyp h in
   match! type t with
     | @eq ?a ?f ?g => 
         let (l, b) := split_prod a in
-        gen_eq t (List.rev l) b f g [] ()
+        gen_eq h (List.rev l) b f g [] () ;
+        ltac1:(H |- clear H) (Ltac1.of_constr t)
   end.
-        
-  
-        
-        (* 
-        let new_eq := make (Prod bind 
-        
 
-
-
-
-        let f' := (liftn 1 0 f) in
-        let g' := (liftn 1 0 g) in
-        (Prod bind (make (gen_eq l b f' g' (0 :: (List.map (fun x => Int.add x 1) rels))))) 
-  end.
- 
-Ltac2 rec gen_eq (l : binder list) (b: constr) (f: constr) (g: constr) (rels: int list) :=
-  match l with
-    | [] => 
-        let vars := Array.map (fun x => make (Rel x)) (Array.of_list rels) in
-        let arr := Array.of_list [b; make (App f vars); make (App g vars)] in
-        (App constr:(@eq) arr)
-    | bind :: l =>
-        let f' := (liftn 1 0 f) in
-        let g' := (liftn 1 0 g) in
-        (Prod bind (make (gen_eq l b f' g' (0 :: (List.map (fun x => Int.add x 1) rels))))) 
-  end.
-     *)
-(* constr list -> int -> constr -> constr *)
+Tactic Notation "eliminate_ho_eq" ident(H) := 
+  let f := ltac2:(id |- 
+  let id2 := Ltac1.to_ident id in 
+  let id3 := match id2 with 
+      | None => Control.throw (Not_found)
+      | Some id3 => id3 
+    end in eliminate_ho_eq id3) in f H.
 
 Section tests.
 
-Variable (f : forall (A B : Type) (a : A) (b : B), A * B).
-Variable (g : forall (A B : Type) (a : A) (b : B), A * B).
-Variable (Heq : f = g).  
+Set Default Proof Mode "Classic".
 
-Goal False. 
+Goal forall (f g : forall (A B : Type) (a : A) (b : B), A * B) (Heq : f = g), False.
+intros f g Heq. 
 assert (H : length =
 fun A : Type =>
 fix length (l : list A) : nat :=
   match l with
   | nil => 0
   | (_ :: l')%list => S (length l')
-  end) by reflexivity.  eliminate_ho_eq 'H. intro. intro. reflexivity.
+  end) by reflexivity. eliminate_ho_eq H.  
 assert (H1 : Nat.add =
 fix add (n m : nat) {struct n} : nat :=
   match n with
